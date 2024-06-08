660066eb4d1f8e5074ae1f6decfb7cc7dff765b3
TeatrumMundi <michal3659@gmail.com>
2024-06-08 15:04:23 +0200

    Zad2 Done

--- Programowanie_NET/Program.cs
+++ Programowanie_NET/Program.cs
@@ -1,31 +1,19 @@
-﻿using System;
-
-namespace Programowanie_NET
+﻿namespace Programowanie_NET
 {
-    internal class Program
+    internal static class Program
     {
         public static void Main(string[] args)
         {
-            Samochod s1 = new Samochod();
-            
-            s1.WypiszInfo();
-            
-            s1._setMarka("Fiat");
-            s1._setModel("126p");
-            s1._setIloscDrzwi(2);
-            s1.SetPojemnoscSilnika(650);
-            s1.SetSrednieSpalanie(6.0);
-            
-            s1.WypiszInfo();
-            
+            Samochod s1 = new Samochod("Fiat", "126p", 2, 650, 6.0);
             Samochod s2 = new Samochod("Syrena", "105", 2, 800, 7.6);
             
-            s2.WypiszInfo();
-            
-            double kosztPrzejazdu = s2.ObliczKosztPrzejazdu(30.5, 4.85);
-            Console.WriteLine("Koszt przejazdu: " + kosztPrzejazdu);
-            
-            Console.ReadKey();
+            Garaz g1 = new Garaz("ul. Garażowa 1", 1);
+
+            g1.WprowadzSamochod(s1);
+            g1.WyprowadzSamochod();
+            g1.WprowadzSamochod(s2);
+
+            g1.WypiszInfo();
         }
     }
 }
