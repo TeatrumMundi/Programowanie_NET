835ddb2939e7d05ac51919ba1028a1910ee71df1
TeatrumMundi <michal3659@gmail.com>
2024-06-09 12:45:46 +0200

    ZAD6 Done

--- Programowanie_NET/Program.cs
+++ Programowanie_NET/Program.cs
@@ -5,38 +5,56 @@ namespace Programowanie_NET
 {
     static class Program
     {
-        static void Main(string[] args)
+        static void Main()
         {
-            // Ścieżka do pliku tekstowego
-            string filePath = "ścieżka/do/pliku.txt";
+            // Ścieżki do plików
+            string sourceFilePath = @"C:\Users\micha\OneDrive\Desktop\test.txt";
+            string destinationFilePath = @"C:\Users\micha\OneDrive\Desktop\test1.txt";
 
             try
             {
-                // Otwieranie pliku i wczytywanie linii
-                using (StreamReader sr = new StreamReader(filePath))
+                // Otwieranie pliku źródłowego do odczytu
+                using (FileStream sourceStream = new FileStream(sourceFilePath, FileMode.Open, FileAccess.Read))
                 {
-                    string line;
-                    while ((line = sr.ReadLine()) != null)
+                    // Sprawdzamy, czy plik źródłowy jest pusty
+                    if (sourceStream.Length == 0)
                     {
-                        // Odwracanie linii
-                        string reversedLine = ReverseString(line);
-                        // Wyświetlanie odwróconej linii
-                        Console.WriteLine(reversedLine);
+                        Console.WriteLine("Plik źródłowy jest pusty.");
+                        return;
+                    }
+
+                    // Otwieranie pliku docelowego do zapisu
+                    using (FileStream destinationStream = new FileStream(destinationFilePath, FileMode.Create, FileAccess.Write))
+                    {
+                        // Bufor do przechowywania danych
+                        byte[] buffer = new byte[4096];
+                        int bytesRead;
+
+                        // Odczytywanie danych z pliku źródłowego i zapisywanie do pliku docelowego
+                        while ((bytesRead = sourceStream.Read(buffer, 0, buffer.Length)) > 0)
+                        {
+                            Console.WriteLine($"Odczytano {bytesRead} bajtów");
+                            destinationStream.Write(buffer, 0, bytesRead);
+                            Console.WriteLine("Zapisano dane do pliku docelowego");
+                        }
+                    }
+
+                    // Sprawdzenie, czy plik docelowy został zapisany
+                    if (File.Exists(destinationFilePath))
+                    {
+                        Console.WriteLine("Plik został zapisany pomyślnie.");
+                    }
+                    else
+                    {
+                        Console.WriteLine("Plik nie został zapisany.");
                     }
                 }
             }
             catch (Exception e)
             {
-                Console.WriteLine("Wystąpił błąd podczas wczytywania pliku:");
+                Console.WriteLine("Wystąpił błąd podczas kopiowania pliku:");
                 Console.WriteLine(e.Message);
             }
-        }
-
-        static string ReverseString(string s)
-        {
-            char[] charArray = s.ToCharArray();
-            Array.Reverse(charArray);
-            return new string(charArray);
         }
     }
 }
